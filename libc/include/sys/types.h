b266fce3e81c2923c87265174d3b519c19cd4289
Elliott Hughes <enh@google.com>
2013-03-08 15:28:52 -0800

    Let bionic use the correct definition of ssize_t.
    Bug: 8253769
    Change-Id: I50c7cc20828fc089b83580e039ce9153a6c5a8cc

--- libc/include/sys/types.h
+++ libc/include/sys/types.h
@@ -93,7 +93,12 @@ typedef unsigned int  size_t;
 /* size_t is defined by the GCC-specific <stddef.h> */
 #ifndef _SSIZE_T_DEFINED_
 #define _SSIZE_T_DEFINED_
-typedef long int  ssize_t;
+/* Traditionally, bionic's ssize_t was "long int". This caused GCC to emit warnings when you
+ * pass a ssize_t to a printf-style function. The correct type is __kernel_ssize_t, which is
+ * "int", which isn't an ABI change for C code (because they're the same size) but is an ABI
+ * change for C++ because "int" and "long int" mangle to "i" and "l" respectively. So until
+ * we can fix the ABI, this change should not be propagated to the NDK. http://b/8253769. */
+typedef __kernel_ssize_t ssize_t;
 #endif
 
 typedef __kernel_suseconds_t  suseconds_t;
